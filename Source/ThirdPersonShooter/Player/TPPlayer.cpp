6408549866a7afe0fa3de4887b9ffad7cc67cbd0
Rud156 <rudrangshu_1997@yahoo.com>
2021-02-26 15:39:22 -0700

    Added New C++ Character

--- Source/ThirdPersonShooter/Player/TPPlayer.cpp
+++ Source/ThirdPersonShooter/Player/TPPlayer.cpp
@@ -3,22 +3,327 @@
 
 #include "./TPPlayer.h"
 
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 ATPPlayer::ATPPlayer()
 {
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.0f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+
 	PrimaryActorTick.bCanEverTick = true;
 }
 
 void ATPPlayer::BeginPlay()
 {
 	Super::BeginPlay();
+
+	PushPlayerMovementState(EPlayerMovementState::Walk);
+	ApplyChangesToCharacter();
+
+	_isLeftShoulder = false;
+	_shoulderStartPosition = CameraRightShoulder;
+	_shoulderEndPosition = CameraRightShoulder;
+	_shoulderCameraLerpAmount = 0;
 }
 
 void ATPPlayer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	UpdateCapsuleSize(DeltaTime);
+	UpdateShoulderCamera(DeltaTime);
+	UpdateDive(DeltaTime);
 }
 
 void ATPPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	check(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", EInputEvent::IE_Pressed, this, &ATPPlayer::JumpCharacter);
+	PlayerInputComponent->BindAction("Sprint", EInputEvent::IE_Pressed, this, &ATPPlayer::HandleSprintPressed);
+	PlayerInputComponent->BindAction("Crouch", EInputEvent::IE_Pressed, this, &ATPPlayer::HandleCrouchPressed);
+	PlayerInputComponent->BindAction("ShoulderSwap", EInputEvent::IE_Pressed, this, &ATPPlayer::HandleShoulderSwapPressed);
+	PlayerInputComponent->BindAction("Dive", EInputEvent::IE_Pressed, this, &ATPPlayer::HandleDivePressed);
+
+	PlayerInputComponent->BindAxis("Turn", this, &ATPPlayer::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUp", this, &ATPPlayer::LookUpRate);
+	PlayerInputComponent->BindAxis("MoveForward", this, &ATPPlayer::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ATPPlayer::MoveRight);
+}
+
+void ATPPlayer::MoveForward(const float Value)
+{
+	_verticalInput = Value;
+	if (GetTopPlayerState() == EPlayerMovementState::Dive)
+	{
+		return;
+	}
+
+	if (Controller != nullptr && Value != 0.0f)
+	{
+		const FRotator rotation = GetControlRotation();
+		const FRotator yawRotation(0, rotation.Yaw, 0);
+
+		const FVector direction = FRotationMatrix(yawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(direction, Value);
+	}
+
+	if (GetTopPlayerState() == EPlayerMovementState::Run)
+	{
+		if (Value != 1)
+		{
+			RemovePlayerMovementState(EPlayerMovementState::Run);
+			ApplyChangesToCharacter();
+		}
+	}
+}
+
+void ATPPlayer::MoveRight(const float Value)
+{
+	_horizontalInput = Value;
+	if (GetTopPlayerState() == EPlayerMovementState::Dive)
+	{
+		return;
+	}
+
+	if (Controller != nullptr && Value != 0.0f)
+	{
+		const FRotator rotation = Controller->GetControlRotation();
+		const FRotator yawRotation(0, rotation.Yaw, 0);
+
+		const FVector direction = FRotationMatrix(yawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(direction, Value);
+	}
+}
+
+void ATPPlayer::LookUpRate(const float Value)
+{
+	AddControllerPitchInput(Value * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ATPPlayer::TurnAtRate(const float Value)
+{
+	AddControllerYawInput(Value * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ATPPlayer::JumpCharacter()
+{
+	if (GetTopPlayerState() == EPlayerMovementState::Crouch)
+	{
+		RemovePlayerMovementState(EPlayerMovementState::Crouch);
+		ApplyChangesToCharacter();
+		return;
+	}
+
+	Jump();
+}
+
+void ATPPlayer::HandleSprintPressed()
+{
+	RemovePlayerMovementState(EPlayerMovementState::Crouch);
+	PushPlayerMovementState(EPlayerMovementState::Run);
+	ApplyChangesToCharacter();
+}
+
+void ATPPlayer::HandleCrouchPressed()
+{
+	if (GetTopPlayerState() == EPlayerMovementState::Crouch)
+	{
+		RemovePlayerMovementState(EPlayerMovementState::Crouch);
+	}
+	else
+	{
+		PushPlayerMovementState(EPlayerMovementState::Crouch);
+	}
+
+	ApplyChangesToCharacter();
+}
+
+void ATPPlayer::HandleShoulderSwapPressed()
+{
+	_isLeftShoulder = !_isLeftShoulder;
+	_shoulderStartPosition = FollowCamera->GetRelativeLocation();
+
+	if (_isLeftShoulder)
+	{
+		_shoulderEndPosition = CameraLeftShoulder;
+	}
+	else
+	{
+		_shoulderEndPosition = CameraRightShoulder;
+	}
+	_shoulderCameraLerpAmount = 0;
+}
+
+void ATPPlayer::UpdateShoulderCamera(const float DeltaTime)
+{
+	if (_shoulderCameraLerpAmount > 1 || _shoulderCameraLerpAmount < 0)
+	{
+		return;
+	}
+
+	const FVector mappedLocation = FMath::Lerp(_shoulderStartPosition, _shoulderEndPosition, _shoulderCameraLerpAmount);
+	FollowCamera->SetRelativeLocation(mappedLocation);
+
+	_shoulderCameraLerpAmount += CameraLerpSpeed * DeltaTime;
+
+	if (_shoulderCameraLerpAmount >= 1)
+	{
+		FollowCamera->SetRelativeLocation(_shoulderEndPosition);
+	}
+}
+
+void ATPPlayer::HandleDivePressed()
+{
+	const FVector direction = FVector(-_horizontalInput, _verticalInput, 0);
+	_diveDirection = direction;
+	_diveTimer = DiveDuration;
+
+	RemovePlayerMovementState(EPlayerMovementState::Crouch);
+	RemovePlayerMovementState(EPlayerMovementState::Run);
+	PushPlayerMovementState(EPlayerMovementState::Dive);
+	ApplyChangesToCharacter();
+}
+
+void ATPPlayer::UpdateDive(const float DeltaTime)
+{
+	if (_diveTimer > 0)
+	{
+		_diveTimer -= DeltaTime;
+		if (_diveTimer <= 0)
+		{
+			RemovePlayerMovementState(EPlayerMovementState::Dive);
+			ApplyChangesToCharacter();
+		}
+		else
+		{
+			AddMovementInput(_diveDirection, 1);
+		}
+	}
+}
+
+void ATPPlayer::SetCapsuleData(float TargetHeight, float TargetRadius)
+{
+	_capsuleHeight = FVector2D(TargetHeight, GetCapsuleComponent()->GetUnscaledCapsuleHalfHeight());
+	_capsuleRadius = FVector2D(TargetRadius, GetCapsuleComponent()->GetUnscaledCapsuleRadius());
+	_capsuleLerpAmount = 0;
+}
+
+void ATPPlayer::UpdateCapsuleSize(float DeltaTime)
+{
+	if (_capsuleLerpAmount > 1 || _capsuleLerpAmount < 0)
+	{
+		return;
+	}
+
+	const float currentHeight = FMath::Lerp(_capsuleHeight.Y, _capsuleHeight.X, _capsuleLerpAmount);
+	const float currentRadius = FMath::Lerp(_capsuleRadius.Y, _capsuleRadius.X, _capsuleLerpAmount);
+
+	GetCapsuleComponent()->SetCapsuleHalfHeight(currentHeight);
+	GetCapsuleComponent()->SetCapsuleRadius(currentRadius);
+
+	_capsuleLerpAmount += CapsuleSizeLerpRate * DeltaTime;
+	if (_capsuleLerpAmount > 1)
+	{
+		GetCapsuleComponent()->SetCapsuleHalfHeight(_capsuleHeight.X);
+		GetCapsuleComponent()->SetCapsuleRadius(_capsuleRadius.X);
+	}
+}
+
+void ATPPlayer::PushPlayerMovementState(EPlayerMovementState MovementState)
+{
+	_movementStack.Add(MovementState);
+}
+
+void ATPPlayer::RemovePlayerMovementState(const EPlayerMovementState MovementState)
+{
+	for (int i = _movementStack.Num() - 1; i >= 0; i--)
+	{
+		if (_movementStack[i] == MovementState)
+		{
+			_movementStack.RemoveAt(i);
+		}
+	}
+}
+
+bool ATPPlayer::HasPlayerState(const EPlayerMovementState MovementState)
+{
+	for (int i = 0; i < _movementStack.Num(); i++)
+	{
+		if (_movementStack[i] == MovementState)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+EPlayerMovementState ATPPlayer::GetTopPlayerState() const
+{
+	if (_movementStack.Num() <= 0)
+	{
+		return EPlayerMovementState::None;
+	}
+
+	return _movementStack.Last();
+}
+
+void ATPPlayer::ApplyChangesToCharacter()
+{
+	SetCapsuleData(DefaultHalfHeight, DefaultRadius);
+
+	switch (GetTopPlayerState())
+	{
+	case EPlayerMovementState::None:
+		break;
+
+	case EPlayerMovementState::Walk:
+		GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+		break;
+
+	case EPlayerMovementState::Run:
+		GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+		break;
+
+	case EPlayerMovementState::Crouch:
+		GetCharacterMovement()->MaxWalkSpeed = CrouchSpeed;
+		SetCapsuleData(CrouchHalfHeight, CrouchRadius);
+		break;
+
+	case EPlayerMovementState::Dive:
+		GetCharacterMovement()->MaxWalkSpeed = DiveSpeed;
+		SetCapsuleData(CrouchHalfHeight, CrouchRadius);
+		break;
+
+	default:
+		break;
+	}
+}
+
+float ATPPlayer::GetVerticalInput() const
+{
+	return _verticalInput;
+}
+
+float ATPPlayer::GetHorizontalInput() const
+{
+	return _horizontalInput;
 }
--- Source/ThirdPersonShooter/Player/TPPlayer.h
+++ Source/ThirdPersonShooter/Player/TPPlayer.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "../Utils/Enums.h"
 #include "TPPlayer.generated.h"
 
 UCLASS()
@@ -11,11 +12,111 @@ class THIRDPERSONSHOOTER_API ATPPlayer : public ACharacter
 {
 	GENERATED_BODY()
 
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* FollowCamera;
+
+	float _horizontalInput;
+	float _verticalInput;
+
+	TArray<EPlayerMovementState> _movementStack;
+	void PushPlayerMovementState(const EPlayerMovementState MovementState);
+	void RemovePlayerMovementState(const EPlayerMovementState MovementState);
+	bool HasPlayerState(const EPlayerMovementState MovementState);
+	void ApplyChangesToCharacter();
+
+	FVector2D _capsuleRadius; // X: Target, Y: Current
+	FVector2D _capsuleHeight; // X: Target, Y: Current
+	float _capsuleLerpAmount;
+	void SetCapsuleData(const float TargetHeight, const float TargetRadius);
+	void UpdateCapsuleSize(const float DeltaTime);
+
+	bool _isLeftShoulder;
+	float _shoulderCameraLerpAmount;
+	FVector _shoulderStartPosition;
+	FVector _shoulderEndPosition;
+	void UpdateShoulderCamera(const float DeltaTime);
+
+	float _diveTimer;
+	FVector _diveDirection;
+	void UpdateDive(const float DeltaTime);
+
+	void MoveForward(const float Value);
+	void MoveRight(const float Value);
+	void TurnAtRate(const float Value);
+	void LookUpRate(const float Value);
+	void JumpCharacter();
+	void HandleSprintPressed();
+	void HandleCrouchPressed();
+	void HandleShoulderSwapPressed();
+	void HandleDivePressed();
+
 protected:
 	virtual void BeginPlay() override;
 
 public:
+#pragma region Properties
+
+	UPROPERTY(Category = "Player|Movement", EditAnywhere)
+		float BaseLookUpRate;
+
+	UPROPERTY(Category = "Player|Movement", EditAnywhere)
+		float BaseTurnRate;
+
+	UPROPERTY(Category = "Player|Movement", EditAnywhere)
+		float CrouchSpeed;
+
+	UPROPERTY(Category = "Player|Movement", EditAnywhere)
+		float WalkSpeed;
+
+	UPROPERTY(Category = "Player|Movement", EditAnywhere)
+		float SprintSpeed;
+
+	UPROPERTY(Category = "Player|Movement", EditAnywhere)
+		float DiveSpeed;
+
+	UPROPERTY(Category = "Player|Movement", EditAnywhere)
+		float DiveDuration;
+
+	UPROPERTY(Category = "Player|Camera", EditAnywhere)
+		FVector CameraLeftShoulder;
+
+	UPROPERTY(Category = "Player|Camera", EditAnywhere)
+		FVector CameraRightShoulder;
+
+	UPROPERTY(Category = "Player|Camera", EditAnywhere)
+		float CameraLerpSpeed;
+
+	UPROPERTY(Category = "Player|Size", EditAnywhere)
+		float DefaultHalfHeight;
+
+	UPROPERTY(Category = "Player|Size", EditAnywhere)
+		float DefaultRadius;
+
+	UPROPERTY(Category = "Player|Size", EditAnywhere)
+		float CrouchHalfHeight;
+
+	UPROPERTY(Category = "Player|Size", EditAnywhere)
+		float CrouchRadius;
+
+	UPROPERTY(Category = "Player|Size", EditAnywhere)
+		float CapsuleSizeLerpRate;
+
+#pragma endregion
+
 	ATPPlayer();
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(Category = "Player|Movement", BlueprintCallable, BlueprintPure)
+		EPlayerMovementState GetTopPlayerState() const;
+
+	UFUNCTION(Category = "Player|Movement", BlueprintCallable, BlueprintPure)
+		float GetVerticalInput() const;
+
+	UFUNCTION(Category = "Player|Movement", BlueprintCallable, BlueprintPure)
+		float GetHorizontalInput() const;
 };
